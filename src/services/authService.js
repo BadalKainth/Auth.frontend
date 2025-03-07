705752981a99acde9e65f7ba65477acc65e8743f
Badal Kainth <badalkainth06@gmail.com>
2025-03-07 22:46:06 +0530

    fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,30 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Set base URL
-const API_URL = '/api/auth';
+const API_URL = "/api/auth";
 
 // Set token in headers
-const setAuthToken = token => {
+const setAuthToken = (token) => {
   if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    localStorage.setItem('token', token);
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    localStorage.setItem("token", token);
   } else {
-    delete axios.defaults.headers.common['Authorization'];
-    localStorage.removeItem('token');
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("token");
   }
 };
 
 // Register user
-export const register = async userData => {
+export const register = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, userData, {
-      withCredentials: true
-    });
-    
+    const response = await axios.post(`${API_URL}/register`, userData);
+
     if (response.data.token) {
       setAuthToken(response.data.token);
     }
-    
+
     return response.data;
   } catch (error) {
     throw error.response.data;
@@ -32,16 +30,14 @@ export const register = async userData => {
 };
 
 // Login user
-export const login = async userData => {
+export const login = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, userData, {
-      withCredentials: true
-    });
-    
+    const response = await axios.post(`${API_URL}/login`, userData);
+
     if (response.data.token) {
       setAuthToken(response.data.token);
     }
-    
+
     return response.data;
   } catch (error) {
     throw error.response.data;
@@ -51,9 +47,7 @@ export const login = async userData => {
 // Logout user
 export const logout = async () => {
   try {
-    await axios.get(`${API_URL}/logout`, {
-      withCredentials: true
-    });
+    await axios.get(`${API_URL}/logout`);
     setAuthToken(null);
   } catch (error) {
     throw error.response.data;
@@ -63,10 +57,8 @@ export const logout = async () => {
 // Get current user
 export const getUser = async () => {
   try {
-    setAuthToken(localStorage.getItem('token'));
-    const response = await axios.get(`${API_URL}/me`, {
-      withCredentials: true
-    });
+    setAuthToken(localStorage.getItem("token"));
+    const response = await axios.get(`${API_URL}/me`);
     return response.data;
   } catch (error) {
     throw error.response.data;
